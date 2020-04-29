fdcf075efbea4f2c9a394d92ecb52d448dd6b23a
Osmar da Cunha Filho <osmarcf@gmail.com>
2020-04-28 21:42:19 -0300

    retorno modelandview

--- src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
+++ src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
@@ -2,17 +2,20 @@ package br.edu.usj.ads.lpii.calculadora;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class Calculadora {
 
-    @PostMapping(value="path")
-    public SomeEnityData postMethodName(@RequestBody SomeEnityData entity) {
-        //TODO: process POST request
+    @PostMapping(value="/calcula")
+    public ModelAndView postCalcula(@RequestParam String numero1, @RequestParam String numero2) {
         
-        return entity;
+        ModelAndView modelAndView = new ModelAndView();
+        
+        
+        return modelAndView;
     }
     
 
