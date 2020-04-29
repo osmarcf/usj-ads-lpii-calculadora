02f81365caa3926172716a7408c1c3ff39198b77
Osmar da Cunha Filho <osmarcf@gmail.com>
2020-04-28 21:40:04 -0300

    comeco do metodo postmapping

--- src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
+++ src/main/java/br/edu/usj/ads/lpii/calculadora/Calculadora.java
@@ -1,9 +1,20 @@
 package br.edu.usj.ads.lpii.calculadora;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class Calculadora {
+
+    @PostMapping(value="path")
+    public SomeEnityData postMethodName(@RequestBody SomeEnityData entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
     
+
 
 }
